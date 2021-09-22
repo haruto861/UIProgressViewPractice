225b7b8c9933c1aa524914621469cbdc790de75e
haruto-matsushima <matsu_haruto@icloud.com>
2021-09-22 18:36:53 +0900

    inital commit

---
+++ UIProgressViewPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIProgressViewPractice
+//
+//  Created by 松島悠人 on 2021/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
