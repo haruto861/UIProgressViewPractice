f1ce7a99a2a2dc3302f30d22b72eb2754e28caa1
haruto-matsushima <matsu_haruto@icloud.com>
2021-09-25 13:28:13 +0900

    sample

--- UIProgressViewPractice/ViewController.swift
+++ UIProgressViewPractice/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+    @IBOutlet private weak var progressView: UIProgressView!
+    @IBOutlet private weak var plusButton: UIButton! {
+        didSet {
+            plusButton.addTarget(self, action: #selector(plus), for: .touchUpInside)
+        }
+    }
+    @IBOutlet private weak var minusButton: UIButton! {
+        didSet {
+            minusButton.addTarget(self, action: #selector(minus), for: .touchUpInside)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        generateRandomValue()
+    }
+    
+    @objc private func plus() {
+        progressView.progress += 0.1
     }
 
+    @objc private func minus() {
+        progressView.progress -= 0.1
+    }
 
+    private func generateRandomValue() {
+        let randomFloat = Float.random(in: 0 ..< 1)
+        progressView.progress = randomFloat
+    }
 }
 
